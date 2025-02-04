265633b188dc5cfc67fe66f3603e40dcf91a2ade
Amir-A93 <amir.ali-pour@etsmtl.ca>
2025-02-03 19:06:02 -0500

    clustering logic under development. 2Layer Hierarchical clustering is being implemented.

--- Client/Core/Modules/Coordinator_Modules/clustering_engine.py
+++ Client/Core/Modules/Coordinator_Modules/clustering_engine.py
@@ -1,15 +1,21 @@
 from Core.Modules.Coordinator_Modules.components import Cluster
 from Core.Modules.Coordinator_Modules.components import Cluster_Node
 from Core.Modules.Coordinator_Modules.components import Session
+import math 
 
 class Clustering_Engine():
     def __init__(self):
         return
 
     
-    def create_topology(self,session): #TODO:incorporate 30,70 or 20,80 or ...
+    def create_2layer_topology(self,session,percentage_of_aggs): #TODO:incorporate 30,70 or 20,80 or ...
         session.role_vector = []
         session.role_dictionary = {}
+        num_aggregators = math.floor(session.session_capacity_max * percentage_of_aggs)
+        num_training_only = session.session_capacity_max - num_aggregators
+
+        for i in range(num_aggregators):
+            session.list
         return [[],{}]#TODO:[agg_role_vector, role_dictionary]
  
     def form_clusters(self,session):
