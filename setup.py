f299630345c87b078371ea19a3c118bd34405634
Amir-A93 <amir.ali-pour@etsmtl.ca>
2025-04-03 13:27:30 -0400

    changed version:

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sdflmq',
-    version='0.1.0.4',
+    version='0.1.0.6',
     author='Amir Ali-Pour',
     author_email='alipouramir93@gmail.com',
     description='A semi-decentralized federated learning framework for Python',
