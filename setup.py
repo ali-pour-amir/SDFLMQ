3c5cce6e0242299328f7a23d0ccc7256609b5061
Amir-A93 <amir.ali-pour@etsmtl.ca>
2025-04-03 12:57:43 -0400

    added __init__ in the optimizers directory. Changed version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sdflmq',
-    version='0.1.0.3',
+    version='0.1.0.4',
     author='Amir Ali-Pour',
     author_email='alipouramir93@gmail.com',
     description='A semi-decentralized federated learning framework for Python',
